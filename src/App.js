f58e9191dfa0d12835fc0b0de9f92596f63adc2c
andrewneilclarke <andrewneilclarke@gmail.com>
2022-01-01 21:19:00 +0100

    added fetch result

--- src/App.js
+++ src/App.js
@@ -17,9 +17,12 @@ function App() {
   const [gameOver, setGameOver] = useState(null)
   let [liveDeck, setLiveDeck] = useState([...cardNames])
   const [flipped, setFlipped] = useState(true)
+  const [gameResult, setGameResult] = useState({
+    winners: [],
+    players: [],
+  });
 
   const getCardFace = (card) => Deck[card]
-
 
   useEffect(() => {
     // shuffle(liveDeck);
@@ -34,7 +37,7 @@ function App() {
     console.log(liveDeck)
   }
 
-  const dealFlop = (table, player1, player2) => {
+  const dealFlop = (deck, table, player1, player2) => {
     player1.cards.push(liveDeck.pop())
     player2.cards.push(liveDeck.pop())
     player1.cards.push(liveDeck.pop())
@@ -42,18 +45,52 @@ function App() {
     table.push(liveDeck.pop())
     table.push(liveDeck.pop())
     table.push(liveDeck.pop())
+    setLiveDeck([...liveDeck])
     setGameOver(false)
-    console.log('cards: ', liveDeck, 'table: ', table, 'player1 cards: ', player1.cards)
     return liveDeck, player1, player2
   }
 
-  const dealTurn = (table, burnt, player1, player2) => {
+  const dealTurn = (deck, table, burnt, player1, player2) => {
     burnt.push(liveDeck.pop())
     table.push(liveDeck.pop())
+    setLiveDeck([...liveDeck])
     setGameOver(false)
-    console.log('cards: ', liveDeck, 'table: ', table, 'player1 cards: ', player1.cards)
     return liveDeck, player1, player2, burnt
   }
+
+  const dealRiver = (deck, table, burnt, player1, player2) => {
+    burnt.push(liveDeck.pop())
+    table.push(liveDeck.pop())
+    setLiveDeck([...liveDeck])
+    setGameOver(false)
+    return liveDeck, player1, player2, burnt
+  }
+
+  const getResult = async () => {
+    const r = await fetchRankingResult
+    setGameResult(r)
+  }
+
+  const fetchRankingResult = async () => {
+    const finaltable = table.join(',')
+    // console.log(table)
+    let players = [player1, player2]
+    players = [players.map((p) => `&pc[]=${p.cards.join(',')}`).join('')]
+    console.log('table: ', table, 'players', players)
+
+    // const table = game.table.join(',');
+
+    // table:  9H,3S,2D,8C,2C players:  &pc[]=QS,QH&pc[]=7D,7H&pc[]=JD,AH
+    // const players = game.players.map((p) => `&pc[]=${p.hand.join(',')}`).join('');
+    let url = `https://api.pokerapi.dev/v1/winner/texas_holdem?cc=${finaltable}${players}`;
+    // let url =
+    //   'https://api.pokerapi.dev/v1/winner/texas_holdem?cc=AC,KD,QH,JS,7C&pc[]=10S,8C&pc[]=3S,2C&pc[]=QS,JH';
+    const res = await fetch(url);
+    // console.log(res.json())
+    return res.json();
+  };
+
+  // const equalHand = (p: Player) => (r: Result) => r.cards === p.hand.join(',');
 
   return (
     <div className="App">
@@ -63,20 +100,24 @@ function App() {
           <Card key={card} card={card} getCardFace={getCardFace} cardBack={cardBack} flipped={flipped} setFlipped={setFlipped} />
         </div>)
         )}
-      </div>
-      <div className="burnt">
-        <p>Burnt</p>
-        {burnt.map(card =>
-        (<div id="deck">
-          <Card key={card} card={card} getCardFace={getCardFace} cardBack={cardBack} flipped={flipped} setFlipped={setFlipped} />
-        </div>))}
+        <div className="burnt">
+          <p id="burnt">Burnt: </p>
+          <div className="burnt-cont">
+            {burnt.map(card =>
+            (<div id="deck">
+              <Card key={card} card={card} getCardFace={getCardFace} cardBack={cardBack} flipped={flipped} setFlipped={setFlipped} />
+            </div>))}
+          </div>
+        </div>
       </div>
 
       <h3>Poker Fools</h3>
       <div className="buttons">
         <button onClick={shuffleandSet}>Shuffle</button>
-        <button onClick={() => dealFlop(table, player1, player2)}>Deal Flop</button>
-        <button onClick={() => dealTurn(table, burnt, player1, player2)}>Deal Turn</button>
+        <button onClick={() => dealFlop(liveDeck, table, player1, player2)}>Deal Flop</button>
+        <button onClick={() => dealTurn(liveDeck, table, burnt, player1, player2)}>Deal Turn</button>
+        <button onClick={() => dealRiver(liveDeck, table, burnt, player1, player2)}>Deal River</button>
+        <button onClick={fetchRankingResult}>Result</button>
       </div>
       <div className="table-and-players">
         <div className="table">
@@ -113,6 +154,7 @@ function App() {
               <p>{JSON.stringify(player2.cards)}</p>
               <p>{JSON.stringify(player2.stack)}</p>
             </div>
+            {gameResult && <p>{JSON.stringify(gameResult)}</p>}
           </div>
         </div>
       </div>

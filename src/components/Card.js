44af3c6d4c3e3b4a4a8294c73c2c5b07ad106924
andrewneilclarke <andrewneilclarke@gmail.com>
2022-01-10 17:03:11 +0100

    minor change

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
 import '../Card.css'
+import { useState } from 'react'
 
 const Card = ({ card, getCardFace, cardBack, flipped, setFlipped }) => {
+    const [isFlipped, setIsFlipped] = useState(flipped)
 
+    // helper funtion to check card colour
     const checkColour = (card) => {
-
         const cardArr = card.toLowerCase().split('')
         const hasRed = cardArr.filter(item =>
             item === 'h' || item === 'd'
@@ -12,15 +14,18 @@ const Card = ({ card, getCardFace, cardBack, flipped, setFlipped }) => {
     }
 
     const flipCard = (card) => {
-        console.log(card)
-        setFlipped(!flipped)
+        setIsFlipped(!isFlipped)
     }
+    // const flipCard = (card) => {
+    //     // console.log(card)
+    //     setFlipped(!flipped)
+    // }
 
     return (
         <div className="card">
             <div className={flipped ? "flipped" : ""}>
-                {flipped ? <p className={checkColour(card) ? "front red" : "front"} onClick={() => flipCard(card)}> {getCardFace(card)}</p> :
-                    // <p className="back" id="cardback">{cardBack}</p>
+                {isFlipped ?
+                    <p className={checkColour(card) ? "front red" : "front"} onClick={() => flipCard(card)}> {getCardFace(card)}</p> :
                     <p className='cardback' onClick={() => flipCard(card)}>{cardBack}</p>
                 }
 
